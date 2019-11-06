47b34ac15c852915f5b9f0bef7e5f462f7c36e60
vuthaouet <55568077+vuthaouet@users.noreply.github.com>
2019-11-06 22:52:27 +0700

    ltinh

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 // const bodyParser = require('body-parser');
 const app = express();
 const db = require('./models/index');
